95e48338c9bce94c43812b0329e7e25098eb4d09
Celeste Acosta <89424776+ca3-10@users.noreply.github.com>
2021-09-05 04:58:16 +0000

    finish tests cases(assert stack.print())

--- 002/code.py
+++ 002/code.py
@@ -1,4 +1,5 @@
 from helperfunctions import convert_to_numbers
+from helperfunctions import convert_to_letters
 
 def encode(string, a, b):
     trivial_encode = convert_to_numbers(string)
@@ -6,3 +7,73 @@ def encode(string, a, b):
     for nums in trivial_encode: 
         linear_encode.append(nums * a + b)
     return linear_encode
+
+
+def decode(number, a, b):
+    basic_list = []
+    for nums in number:
+        basic_list.append((nums - b)/a)
+    if max(basic_list) >= 27:
+        return False
+    return convert_to_letters(basic_list)
+
+
+def calc_nth_root(num,root,presicion):
+    upper_bound = num
+    lower_bound = 0
+    while presicion > 0: 
+        guess = (upper_bound + lower_bound)/2
+        if guess ** root < num: 
+            lower_bound = guess 
+        elif guess ** root > num:
+            upper_bound = guess
+        presicion -= 1 
+    return guess
+
+
+def calc_minimum(numbers):
+    minimum = numbers[0]
+    for num in numbers:
+        if num < minimum:
+            minimum = num
+    return minimum
+
+
+def simple_sort(numbers):
+    sorted_list = []
+    while len(numbers) > 0:
+        miniMum = calc_minimum(numbers)
+        sorted_list.append(miniMum)
+        numbers.remove(miniMum)
+    return sorted_list
+
+
+class Stack:
+    def __init__(self):
+      self.elements = []
+    
+    def print(self):
+        print(self.elements)
+
+    def push(self,value):
+        self.elements.append(value)
+    
+    def pop(self):
+        self.elements.pop()
+
+
+class Queue: 
+    def __init__(self):
+        self.items = []
+    
+    def print(self):
+        print(self.items)
+
+    
+    def enqueue(self,item):
+        self.items.append(item)
+    
+    def dequeue(self):
+        self.items.pop(0)
+
+
--- 002/helperfunctions.py
+++ 002/helperfunctions.py
@@ -13,3 +13,22 @@ def convert_to_numbers(input_string):
 
         number_list.append(alphabet[letters])
     return number_list
+
+
+def convert_to_letters(input_array):
+    numbers_to_letters_dict = {0: " ", 1: "a", 2: "b", 3: "c", 4: "d", 5: "e",
+                               6: "f", 7: "g", 8: "h", 9: "i", 10: "j",
+                               11: "k", 12: "l", 13: "m", 14: "n", 15: "o",
+                               16: "p", 17: "q",  18: "r", 19: "s", 20: "t",
+                               21: "u", 22: "v", 23: "w", 24: "x", 25: "y",
+                               26: "z"}
+
+    word_list = []
+    word = ""
+
+    for numbers in input_array:
+        if numbers > 27 or numbers < 0:
+            return "Only numbers 0-26"
+            break
+        word_list.append(numbers_to_letters_dict[numbers])
+    return (word.join(word_list))
